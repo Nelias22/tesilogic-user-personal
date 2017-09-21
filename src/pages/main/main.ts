75c97a2b2b02acf6a0904d5ff6b699889d56fcbf
Nayib Elías <nelias@uninorte.edu.co>
2017-09-21 17:57:33 -0500

    quitando un mensaje de debug

--- src/pages/main/main.ts
+++ src/pages/main/main.ts
@@ -57,7 +57,7 @@ export class MainPage {
        		{
        			let newCenter= new google.maps.LatLng(position.coords.latitude, position.coords.longitude)
        			marker.positon= newCenter;
-       			console.log("new center"+ newCenter);
+       			//console.log("new center"+ newCenter);
        		}
        	);
  
